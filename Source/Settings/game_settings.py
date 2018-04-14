a17529cec17bc7058f2320030a4a625f9ab59bb2
Dingcheng Li <lidingcheng@baidu.com>
2018-04-14 12:08:35 -0700

    add Setting

---
+++ Source/Settings/game_settings.py
@@ -0,0 +1,17 @@
+'''
+Game constants which define the game played by DeepStack.
+game_settings
+'''
+def basic_setting():
+    M={}
+    ##the number of card suits in the deck
+    M['suit_count'] = 2
+    ##the number of card ranks in the deck
+    M['rank_count'] = 2
+    ##the total number of cards in the deck
+    M['card_count'] = 2
+    ##the number of public cards dealt in the game (revealed after the first-- betting round)
+    M['board_card_count'] = 1
+    ##the number of players in the game
+    M['player_count'] = 2
+    return M
